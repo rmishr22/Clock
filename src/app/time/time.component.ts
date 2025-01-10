2a5fdd082827779241b18c0dbfde7fb3798178b3
Mishra <ritika.d.mishra@capgemini.com>
2025-01-10 14:41:45 +0530

    minimized code

--- src/app/time/time.component.ts
+++ src/app/time/time.component.ts
@@ -1,25 +1,20 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import moment from 'moment';
 import { interval, map, Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-time',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './time.component.html',
   styleUrl: './time.component.css'
 })
 export class TimeComponent {
   time = '';
-  currentTime:Subscription
-  constructor() {
-    this.currentTime = interval(1000).pipe(
-      map(() => moment().format('h:mm:ss A'))
-    ).subscribe(time => this.time = time)
-  }
-  
-  ngOnDestroy() {
-    if (this.currentTime) {
-      this.currentTime.unsubscribe();
-    }
-  }
+
+  currentTime = interval(1000).pipe(
+    map(() => moment().format('h:mm:ss A'))
+  )
+
 }
