ac2ddbab9b053c3ef7c21e97c55381fc8be9ae1c
Mishra <ritika.d.mishra@capgemini.com>
2025-01-10 09:14:59 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TimeComponent } from './time/time.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TimeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/time/time.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import moment from 'moment';
+import { interval, map } from 'rxjs';
+@Component({
+  selector: 'app-time',
+  standalone: true,
+  imports: [],
+  templateUrl: './time.component.html',
+  styleUrl: './time.component.css'
+})
+export class TimeComponent {
+
+  time = '';
+
+    currentTime = interval(1000).pipe(
+      map(()=>moment().format('h:mm:ss A'))
+    ).subscribe(time => this.time = time )
+}
